e7e9f87504714176d9c457f7837fd7273623a988
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2023-12-18 14:41:34 +0100

    Image toit et titres

--- app/header.jsx
+++ app/header.jsx
@@ -1,4 +1,3 @@
-// TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
 "use client";
 import { useEffect } from "react";
 
@@ -101,15 +100,15 @@ const Header = () => {
     
     <div
       className="bg-neutral-50 px-6 py-20 text-center text-neutral-800 dark:bg-neutral-700 dark:text-neutral-200">
-      <h1 className="mb-6 text-5xl font-bold">Heading</h1>
-      <h3 className="mb-8 text-3xl font-bold">Subeading</h3>
+      <h1 className="mb-6 text-5xl font-bold">PRO COUVREUR ET NETTOYAGE</h1>
+      <h3 className="mb-8 text-3xl font-bold">Loudéac et ses alentours</h3>
       <a
         className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
         data-te-ripple-init
         data-te-ripple-color="light"
         href="#!"
         role="button"
-        >Get started</a
+        >Me contacter</a
       >
     </div>
     <div>
